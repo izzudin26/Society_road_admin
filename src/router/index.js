41ee0ed0e6d9966e9b1e6332fcc14e5c94296312
izzudin26 <izzudin.ar26@gmail.com>
2021-12-26 03:04:44 +0700

    add route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { getToken } from "../webservice/auth";
 
 const routes = [
   {
     path: "/",
-    name: "root",
+    name: "reportRoot",
     component: () => import("../views/adminSidebar.vue"),
     children: [
       { path: "/", component: () => import("../views/admin/report.vue") },
@@ -19,6 +20,11 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name != "Login" && !getToken()) next({ name: "Login" });
+  next();
 });
 
 export default router;
